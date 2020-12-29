57f9d62417e0ffa370b48fdff72fbb560cbe5846
Mongolmaa Gurbazar <mongolmaa555@gmail.com>
2020-12-29 16:40:18 -0500

    header

---
+++ employee_tracker/src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import "./style.css";
+
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Header() {
+    return (
+      <div  className="header">
+        <h1>
+        Employee Directory
+        </h1>
+      </div>
+    );
+  }
+  
+  export default Header;
